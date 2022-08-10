62211a322ebd594028ce8a8bf1115802ba1c4530
Anand raja <meetanandns@gmail.com>
2022-08-10 17:17:26 +0000

    basic working rxjs

--- src/root.component.js
+++ src/root.component.js
@@ -1,4 +1,36 @@
+import React from 'react';
+
+import { getData, state$ } from '@actionanand/utility';
+
 export default function Root(props) {
   console.log(props);
-  return <section>{props.name} is mounted!</section>;
+
+  let angTodo = 'My new Todo';
+
+  React.useEffect(() => {
+    getData('/data').then(data => {
+      console.log('react ', data);
+    });
+
+    const utiSub = state$.subscribe((resp) => {
+      console.log('React ', resp);
+    });
+
+    return () => {
+      utiSub.unsubscribe();
+    }
+
+  });
+
+  const handleClick = () => {
+    const event = new CustomEvent('react', { detail: {angTodo} });
+    window.dispatchEvent(event);
+  }
+
+  return (
+    <div>
+      <section>{props.name} is mounted!</section>
+      <button onClick={handleClick}>cool</button>
+    </div>
+  );
 }
