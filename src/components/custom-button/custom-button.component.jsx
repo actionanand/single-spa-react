104f7de1dc581a9700bdbe9a216fb44796201d41
Anand raja <meetanandns@gmail.com>
2022-08-10 18:31:03 +0000

    cus btn

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import classes from './custom-button.styles.scss';
 
 const CustomButton = ({children, normalBtn, inverted, ...otherProps}) => (
   <button className={`${inverted ? 'inverted': '' } ${normalBtn ? 'normal-btn': '' } custom-button`} {...otherProps}>
